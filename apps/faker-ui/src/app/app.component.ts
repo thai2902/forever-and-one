646044f5e7b80ea8cfc22cfd9106a03782d7cdc5
Thai Le <thai2902@gmail.com>
2018-09-22 21:43:19 +0700

    Add project Faker UI

---
+++ apps/faker-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'forever-and-one-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'faker-ui';
+}

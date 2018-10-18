43a6aa8d5884b06d663c5f5fd0d1b5f956ea692d
Thai Le <thai2902@gmail.com>
2018-10-19 02:37:21 +0700

    Update json-schema-faker

--- apps/faker-ui/src/app/app.component.ts
+++ apps/faker-ui/src/app/app.component.ts
@@ -2,11 +2,24 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { FAKER } from './const/faker-namespace';
 import * as jsc from 'to-json-schema';
 import { SIG } from './const/faker-signature';
-
+import * as faker from 'faker';
+import * as moment from 'moment';
+import jsf from 'json-schema-faker';
 declare var JSONSchemaFaker: any;
 JSONSchemaFaker.extend('faker', function() {
-  return require('faker');
+    // return require('faker');
+    console.log('fakerfaker', faker);
+    
+    return faker;
 });
+
+JSONSchemaFaker.option({
+  minItems: 123
+});
+console.log('-------JSONSchemaFaker', JSONSchemaFaker);
+console.log('-------window', window);
+
+
 
 @Component({
   selector: 'forever-and-one-root',
@@ -54,34 +67,53 @@ export class AppComponent {
       console.log('--> rawSchema',rawSchema);
       this.schema = SchemaInjector.inject(rawSchema);
       console.log('--> schema', this.schema);
-      JSONSchemaFaker.resolve(this.schema).then((sample) => {
-        this.sample = sample;
-        console.log('--> sample', this.sample);
-      });
+      this.generateFromSchema();
+      console.log('--> sample', this.sample);
     } catch (error) {
       console.log(error);
     }
+  }
+
+  updateSchema(event) {
+    try {
+      this.schema = JSON.parse(event);
+    } catch (error) {
+      // this.schema = {error: 'Parse error'}
+    }
+  }
+
+  generateFromSchema() {
+    const _sample = [];
+    for (let i=0; i<50; i++) {
+        _sample.push(JSONSchemaFaker.generate(this.schema))
+    }
+    this.sample = _sample;
   }
 }
 
 export class SchemaInjector {
   static inject(obj: any) {
     const required = [];
-    if (obj.properties) {
+    if (obj.type === 'object' && obj.properties) {
       Object.keys(obj.properties).forEach(k => {
         if (obj.properties[k].type === 'string' || obj.properties[k].type === 'integer') {
-          let type;
+          let type, faker;
           for(let i=0; i<SIG.length; i++) {
             if(new RegExp(SIG[i].regx, 'i').test(k)) {
               type = SIG[i].type;
+              faker = SIG[i].faker;
               break;
             }
           }
-          obj.properties[k]['x-faker'] = type || 'lorem.words';
+          obj.properties[k]['type'] = type;
+          obj.properties[k]['x-faker'] = faker || 'lorem.words';
           required.push(k);
         }
       })
     }
+    // if (obj.type === 'array' && obj.items) {
+    //   return SchemaInjector.inject(obj.items);
+    // }
     obj.required = required;
     return obj;
   }
